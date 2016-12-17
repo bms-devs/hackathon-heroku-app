3298948865238c34b699ed657b2367cef660ab0a
michal.pawlowski <michal.pawowski.3@gmail.com>
2016-12-17 14:18:57 +0100

    Przeniesienie GSONA do DTO, zmiana POST na PUT + POST, cleanup

--- src/main/java/com/github/britter/springbootherokudemo/entity/Room.java
+++ src/main/java/com/github/britter/springbootherokudemo/entity/Room.java
@@ -3,7 +3,6 @@ package com.github.britter.springbootherokudemo.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import com.google.gson.annotations.*;
 import lombok.*;
 
 import java.util.*;
@@ -14,18 +13,14 @@ import java.util.*;
 public class Room {
 
     @Id
-    @Expose
     private long id;
 
-    @Expose
     private String name;
 
     private String token;
 
-    @Expose
     private Boolean occupied;
 
-    @Expose
     private Date lastUpdateDate;
 
     public Room() {
