816acff141800fd1dfa174d286a3e9f381e43af9
Michał Kapiczyński <michal.kapiczynski@bms.com.pl>
2016-12-30 16:19:28 +0100

    [Websocket] - Subscribe Event Listener
    - Dodanie listener'a, który w momencie podłączenia się użytkownika
    ("zgłoszenia chęci subskrypcji") wyśle mu pierwszą wiadomość, bez
    potrzeby oczekiwania na zmianę statusu łazienki

---
+++ src/main/java/com/github/britter/springbootherokudemo/websocket/WebsocketSubscribeEventListener.java
@@ -0,0 +1,18 @@
+package com.github.britter.springbootherokudemo.websocket;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.socket.messaging.*;
+
+@Component
+public class WebsocketSubscribeEventListener implements ApplicationListener<SessionSubscribeEvent> {
+
+    @Autowired
+    private WebSocketService webSocketService;
+
+    @Override
+    public void onApplicationEvent(SessionSubscribeEvent sessionSubscribeEvent) {
+        webSocketService.sendWebsocketNotificationToAllRegisteredClients();
+    }
+}
