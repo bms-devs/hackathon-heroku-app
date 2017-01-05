fe3602279aced7defa83d9315c5fc76a05d5e993
Michał Kapiczyński <michal.kapiczynski@bms.com.pl>
2017-01-05 11:04:03 +0100

    [Websocket] - licznik po stronie klient'a
    - Jako, że klient websocket'woy nie odpytuje już serwera co określony interwał
    czasowy liczenie czasu od ostatniej zmiany po stronie backend'u nei ma sensu.
    Dlatego przeniesiono te logikę na stronę javascriptu.
    
    - Przy inicjalizacji aplikacji, jeśli lastUpdateDate jest null
    ustawiamy lastUpdateDate na czas startu aplikacji,

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,25 +1,30 @@
+window.myScope = {
+    rooms: []
+}
+
 function connect() {
     var socket = new SockJS('/websocketRegister');
     stompClient = Stomp.over(socket);
-    stompClient.connect({}, function(frame) {
+    stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/statusUpdate/update', function(updateMessage) {
+        stompClient.subscribe('/statusUpdate/update', function (updateMessage) {
             updateStatuses(JSON.parse(updateMessage.body))
         });
     });
 }
 
-var updateStatuses = function(result) {
-    result.forEach(function(res) {
-        var imgSrc;
 
+var updateStatuses = function (result) {
+    window.myScope.rooms = [];
+    result.forEach(function (res) {
+        window.myScope.rooms.push(res);
+        var imgSrc;
         if (res.occupied === true) {
             imgSrc = "/circle_red.png";
         } else if (res.occupied === false) {
             imgSrc = "/circle_green.png";
         } else {
             imgSrc = "/unknown.png";
-
         }
 
         var roomEl = $("#" + res.id);
@@ -29,5 +34,25 @@ var updateStatuses = function(result) {
 }
 
 
+function updateClocks() {
+    for (var i = 0; i < window.myScope.rooms.length; i++) {
+        var singleRoom = window.myScope.rooms[i];
+
+        var diff = Math.round((new Date().getTime() - singleRoom.lastUpdateDate) / 1000);
+
+        var d = Math.floor(diff / (24 * 60 * 60));
+        diff = diff - (d * 24 * 60 * 60);
+        var h = Math.floor(diff / (60 * 60));
+        diff = diff - (h * 60 * 60);
+        var m = Math.floor(diff / (60));
+        diff = diff - (m * 60);
+        var s = diff;
+
+        document.getElementById("lastUpdateDate" + singleRoom.id).innerHTML = "od " + h + " h " + m + " m, " + s + " s";
+    }
+}
+
+
+setInterval(updateClocks, 1000);
 
 connect();
